1a5361fb796a42a2d6d51c61aa83b7148a666d5f
平間博人 <hiramahiroto@hiramahiroto-no-MacBook.local>
2017-09-15 12:21:28 +0900

    メール送信

--- SendMail.py
+++ SendMail.py
@@ -1,26 +1,49 @@
 import smtplib
 from email.mime.text import MIMEText
 
+class SendMail:
+    from_adr ="from mailadr"
+    passwd = "password"
+    to_adr = "to mailadr"
+
+    msg = MIMEText("body")
+
+    def __init__(self,adr,pw,to):
+        self.from_adr = adr
+        self.passwd = pw
+        self.to_adr = to
+
+    def sendMail(self):
+        if self.msg == None:
+            print('set msg!')
+            return None
+
+        self.msg['From'] = self.from_adr
+        self.msg['To'] = self.to_adr
+
+        s = smtplib.SMTP('smtp.gmail.com',587)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login(self.from_adr,self.passwd)
+        s.send_message(self.msg)
+        s.close()
+
+    def txtConvertString(self,path):
+        f = open(path,'r')
+        st = f.read()
+        return st
+
+    def setMsg(self,title,body):
+        self.msg = MIMEText(body)
+        self.msg['Subject'] = title
+
 if __name__ == '__main__':
 
-    me = "xxxxxxxxxxx@gmail.com"
-    passwd = "xxxxxxxxxx"
+    fadr = "xxxxxxxxxx@gmail.com" 
+    pwd = "xxxxxxxxxxx"
+    tadr = "xxxxxxxxxx@gmail.com"
 
-    you = "xxxxxxxxxxxx@gmail.com"
-
-    title = "test"
-
-    body = "hello!\nthis mail is test"
-
-    msg = MIMEText(body)
-    msg['Subject'] = title
-    msg['From'] = me
-    msg['To'] = you
-
-    s = smtplib.SMTP('smtp.gmail.com',587)
-    s.ehlo()
-    s.starttls()
-    s.ehlo()
-    s.login(me,passwd)
-    s.send_message(msg)
-    s.close()
+    sm = SendMail(fadr,pwd,tadr)
+    sm.setMsg("test",sm.txtConvertString("text.txt"))
+    sm.sendMail()
